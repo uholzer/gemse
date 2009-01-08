cc773947ed90f66622e041bc6449861193df6d6a
Urs Holzer <urs@andonyar.com>
2009-01-08 21:12:25 +0100

    More progress on insert mode

--- approach1/trivialInsertMode.js
+++ approach1/trivialInsertMode.js
@@ -1,10 +1,16 @@
 
-function AttributeMode(editor, equationEnv, element) {
-    // The attribute mode is specially created for one element!
+function AttributeMode(editor, equationEnv, inElement, beforeElement) {
+    // This insert mode inserts children into inElement, before the
+    // silbing beforeElement. If beforeElement is null, it adds
+    // children to the end of the inElement.
     this.name = "insert (trivial)";
     this.editor = editor;
     this.equationEnv = equationEnv;
     this.element = element;
+    this.cursor = {
+        inElement: inElement,
+        beforeElement: beforeElement
+    };
     this.init = function() {
 
     }
@@ -12,11 +18,36 @@ function AttributeMode(editor, equationEnv, element) {
         // TODO: Clean up attribute mess
         this.equationEnv.finishMode();
     }
-    this.moveCursor = function(index) {
-        this.cursor = ; // TODO
+    this.hideCursor = function() {
+        this.cursor.inElement.removeAttributeNS(NS_internal,"selected");
+        if (this.cursor.beforeElement) {
+            this.cursor.beforeElement.removeAttributeNS(NS_internal,"selected");
+            if (this.cursor.beforeElement.previousSibling) {
+                this.cursor.beforeElement.previousSibling.removeAttributeNS(NS_internal,"selected");
+            }
+        }
+        else if (this.cursor.inElement.lastChild) {
+            this.cursor.inElement.lastChild.removeAttribute(NS_internal,"selected");
+        }
+    }
+    this.showCursor = function() {
+        this.cursor.inElement.addAttributeNS(NS_internal,"selected","insertCursorIn");
+        if (this.cursor.beforeElement) {
+            this.cursor.beforeElement.addAttributeNS(NS_internal,"selected","insertCursorBefore");
+            if (this.cursor.beforeElement.previousSibling) {
+                this.cursor.beforeElement.previousSibling.addAttributeNS(NS_internal,"selected","insertCursorAfter");
+            }
+        }
+        else if (this.cursor.inElement.lastChild) {
+            this.cursor.inElement.lastChild.addAttribute(NS_internal,"selected","insertCursorAfter");
+        }
+    }
+    this.moveCursor = function(newCursor) {
+        this.hideCursor();
+        this.cursor = newCursor; // TODO
+        this.showCursor()
         this.equationEnv.updateViews();
     }
-    this.cursor = null;
     this.__defineGetter__("contextNode", function() { /*TODO*/ }); // TODO
     this.keyHandler = function(event) { standardKeyHandler(event,this.editor) }
     this.inputHandler = function() {
@@ -27,48 +58,95 @@ function AttributeMode(editor, equationEnv, element) {
             this.editor.inputBuffer = "";
             return;
         }
-        commandObject = attributeModeCommands[command];
+        commandObject = attributeModeCommands[command[1]];
         if (commandObject) {
-            commandObject.execute(this)
+            commandObject.execute(this,command[1])
         }
         else {
             throw "Command not found";
         }
     };
+    this.putElement(ns, name) {
+        if (ns) { throw "putElement does not yet support namespaces" }
+        // Create the new element
+        newElement = document.createElement(name);
+        for (var i = 2; i < arguments.length; ++i) {
+            newElement.appendChild(arguments[i]);
+        }
+        // Put element into the equation
+        this.cursor.inElement.insertBefore(newElement, this.cursor.beforeElement);
+        // Position the cursor
+        // TODO: Where?
+    }
 }
 
 trivialInsertModeCommands = {
-
+    "i": {
+        execute: trivialInsertModeCommand_miSingle
+    }
+    "I": {
+        execute: trivialInsertModeCommand_miLong
+    }
+    "n": {
+        execute: trivialInsertModeCommand_mnNormal
+    }
+    "N": {
+        execute: trivialInsertModeCommand_mnLong
+    }
+    "o": {
+        execute: trivialInsertModeCommand_moNormal
+    }
+    "O": {
+        execute: trivialInsertModeCommand_moLong
+    }
+    "t": {
+        execute: trivialInsertModeCommand_mtext
+    }
 }
 
-function trivialInsertModeCommand_miSingle() {
+function trivialInsertModeCommand_miSingle(mode) {
     // Inserts an mi element with a single character as content
 }
 
-function trivialInsertModeCommand_miLong() {
+function trivialInsertModeCommand_miLong(mode) {
     // Inserts an mi element with several characters as content
 }
 
-function trivialInsertModeCommand_mnNormal() {
+function trivialInsertModeCommand_mnNormal(mode) {
     // Inserts a mn element, containg a number of the form /^[+-]?[0-9.]+$/
 }
 
-function trivialInsertModeCommand_mnLong() {
+function trivialInsertModeCommand_mnLong(mode) {
     // Inserts an mn element containing anything
-
 }
 
-function trivialInsertModeCommand_moNormal() {
+function trivialInsertModeCommand_moNormal(mode) {
     // Inserts an mo element with a single character as content
 }
 
-function trivialInsertModeCommand_moLong() {
+function trivialInsertModeCommand_moLong(mode) {
     // Inserts an mi element with several characters as content
 }
 
-function trivialInsertModeCommand_mtext() {
+function trivialInsertModeCommand_mtext(mode) {
     // Inserts an mtext element
 }
 
+function trivialInsertModeCommandTool_elementWithSingleCharacter(mode,elementName) {
+    if (mode.editor.inputBuffer.length < 2) { return false }
+    mode.putElement(null, elementName, document.createTextNode(mode.editor.inputBuffer[1]));
+    mode.editor.inputBuffer = "";
+    return true;
+}
 
+function trivialInsertModeCommandTool_elementWithLongText(mode,elementName) {
+    if (mode.editor.inputBuffer[mode.editor.inputBuffer.length-1] != "\n") { return false }
+    mode.putElement(
+        null, 
+        elementName,
+        document.createTextNode(
+            mode.editor.inputBuffer.substring(1,mode.editor.inputBuffer.length-1)
+        )
+    );
+}
 
