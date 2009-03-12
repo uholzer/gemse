f8e784c25ede4dcdd8e1c0794c30ef39c1f81936
Urs Holzer <urs@andonyar.com>
2009-03-12 13:55:56 +0100

    More commands for ucdInsertMode
    The command table of the ucd insert mode has been extended with more
    commands from the trivial insert mode. Some are still missing, because
    I have difficulty to find good key bindings for them.

--- approach1/ucdInsertMode_commandTable.js
+++ approach1/ucdInsertMode_commandTable.js
@@ -21,6 +21,21 @@ ucdInsertModeCommands = {
         execute: trivialInsertModeCommand_cursorJump
     },
 }
+ucdInsertModeCommands[KEYMOD_CONTROL + "t"] = {
+    execute: trivialInsertModeCommand_mtext
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "e"] = {
+    execute: function ucdInsertModeCommand_menclose (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"menclose") }
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "r"] = {
+    execute: function ucdInsertModeCommand_msqrt (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"msqrt") }
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "R"] = {
+    execute: function ucdInsertModeCommand_mroot (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"mroot") }
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "f"] = {
+    execute: function ucdInsertModeCommand_mfenced (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"mfenced") }
+}
 ucdInsertModeCommands[KEYMOD_CONTROL + "u"] = {
     execute: function ucdInsertModeCommand_munder (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"munder") }
 }
@@ -30,8 +45,23 @@ ucdInsertModeCommands[KEYMOD_CONTROL + "v"] = {
 ucdInsertModeCommands[KEYMOD_CONTROL + "U"] = {
     execute: function ucdInsertModeCommand_munderover (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"munderover") }
 }
+ucdInsertModeCommands[KEYMOD_CONTROL + "m"] = {
+    execute: function ucdInsertModeCommand_mmultiscripts (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"mmultiscripts") }
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "p"] = {
+    execute: function ucdInsertModeCommand_mprescripts (mode,command) { return trivialInsertModeCommand_insertDescribedElement(mode,command,"mprescripts") }
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "t"] = {
+    execute: trivialInsertModeCommand_table // For inserting table, tr or td elements
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "T"] = {
+    execute: trivialInsertModeCommand_mlabeledtr
+}
 ucdInsertModeCommands[KEYMOD_CONTROL + "h"] = {
     execute: trivialInsertModeCommand_oneMoreToSurround
+}
+ucdInsertModeCommands[KEYMOD_CONTROL + "l"] = {
+    execute: trivialInsertModeCommand_oneLessToSurround
 }
 ucdInsertModeCommands[String.fromCharCode(0x1b)] = { // Escape
     execute: ucdInsertModeCommand_exit
