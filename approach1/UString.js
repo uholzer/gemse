1d6c2cc8ef77a4e82eab415ba2fdf8e588f1a4f4
Urs Holzer <urs@andonyar.com>
2009-03-20 19:19:26 +0100

    UString integrated in String
    It has turned out that using separate objects for UString and String
    is bad, since while looking at the code, one is never sure of which
    kind an object actually is. Even more so, because the methods of
    UString mostly return string values.
    
    Now, a much better solution has been implemented. The methods of
    that where before methods of UString.prototype have been added to the
    prototype of String directly, using another name (which begins with u)
    in order to not clash with the UTF16 based variants. So, both, the
    UTF16 based functionality and the Unicode character based one are
    always available on every string.

--- approach1/UString.js
+++ approach1/UString.js
@@ -1,152 +1,143 @@
+/* UString is an extension of the prototype of String. It adds some
+ * useful methods which treat the string as a sequence of Unicode
+ * characters instead of a sequence of UTF16 characters. */
 
-function UString(s) {
-    // s must be a string value or a String object, containing
-    // well formed 16 bit characters
-    this.value = s.toString();
-    // Check whether the string is well formed and count the 
-    /// length at the same time
-    this.length = 0;
-    for (var i=0; i<this.value.length; ++i) {
-        if (UString.isSurrogate(this.value[i])==2) {
-            // This is a high surrogate, so a low one must follow
-            if (UString.isSurrogate(this.value[i+1])!=1) {
-                throw "Malformed string: A low surrogate must follow a high one";
+String.prototype.__defineGetter__("uLength", function() {
+    // This getter has runtime o(n) when it is first called and O(1)
+    // if it has already been called at least once for this object.
+    if (this.uLength_alreadyKnown === undefined) {
+        this.uLength_alreadyKnown = 0;
+        for (var i=0; i<this.length; ++i) {
+            if (String.isSurrogate(this[i])==2) {
+                // This is a high surrogate, so a low one must follow
+                if (String.isSurrogate(this[i+1])!=1) {
+                    throw "Malformed string: A low surrogate must follow a high one";
+                }
+                ++this.uLength_alreadyKnown;
             }
-            ++this.length;
-        }
-        else if (UString.isSurrogate(this.value[i])==1) {
-            // This is a low surrogate, so a hogh one must precede
-            if (UString.isSurrogate(this.value[i-1])!=2) {
-                throw "Malformed string: A high surrogate must precede a low one";
-            }
-            // Do not increase length here!
-        }
-        else {
-            ++this.length;
-        }
-    }
-}
-UString.prototype = {
-    toString: function() {
-        return this.value.toString();
-    },
-    valueOf: function() {
-        return this.value.toString();
-    },
-    toUString: function() {
-        // This is just for convenience, when someone calls toUString
-        // on a string without knowing that it is already an UString.
-        return this;
-    },
-    charAt: function(index) {
-        // Returns a string value
-        var internalIndex = this.indexToInternalIndex(index);
-        if (UString.isSurrogate(this.value.charAt(internalIndex))) {
-            return this.value.substring(internalIndex,internalIndex+2);
-        }
-        else {
-            return this.value.charAt(internalIndex);
-        }
-    },
-    charCodeAt: function(index) {
-        // Returns an integer
-        var internalIndex = this.indexToInternalIndex(index);
-        if (UString.isSurrogate(this.value.charAt(internalIndex))) {
-            var hi = this.value.charCodeAt(internalIndex);  
-            var low = this.value.charCodeAt(internalIndex+1);  
-            return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;  
-        }
-        else {
-            return this.value.charCodeAt(internalIndex);
-        }
-    },
-    concat: String.prototype.concat,
-    indexOf: function(searchString, position) {
-        var internalIndex = this.value.indexOf(searchString, position);
-        if (internalIndex == -1) { return -1 }
-        else { return this.internalIndexToIndex(internalIndex) }
-    },
-    lastIndexOf: function(searchString, position) {
-        var internalIndex = this.value.lastIndexOf(searchString, position);
-        if (internalIndex == -1) { return -1 }
-        else { return this.internalIndexToIndex(internalIndex) }
-    },
-    slice: function(start, end) {
-        if (end === undefined) { end = this.length }
-        if (isNaN(start)) { start = 0 }
-        if (isNaN(end)  ) { end   = 0 }
-        if (start < 0) { start = this.length + start }
-        if (end   < 0) { end   = this.length + end   }
-        if (start < 0) { start = 0 }
-        if (end   < 0) { end   = 0 }
-        if (start > this.length) { start = this.length }
-        if (end   > this.length) { end   = this.length }
-        var internalStart = this.indexToInternalIndex(start);
-        var internalEnd = this.indexToInternalIndex(end);
-        return this.value.slice(internalStart, internalEnd);
-    },
-    substring: function(start, end) {
-        if (end === undefined) { end = this.length }
-        if (isNaN(start) || start<0) { start = 0 }
-        if (isNaN(end)   || end<0  ) { end   = 0 }
-        if (start > this.length) { start = this.length }
-        if (end   > this.length) { end   = this.length }
-        var internalStart = this.indexToInternalIndex(start);
-        var internalEnd = this.indexToInternalIndex(end);
-        return this.value.substring(internalStart, internalEnd);
-    },
-
-    
-    /* For private use */
-    indexToInternalIndex: function(index) {
-        // For a given index, the corresponding index in the internal
-        // string this.value is returned.
-        // If the character at index is from a higher plane, the
-        // internal index of its first surrogate is returned, i.e.
-        // the index of its high surrogate
-        // If index is less than 0, an error is thrown
-        // If index is larger than this.length, an error is
-        // thrown
-        if (index < 0) { throw "negative index not accepted" }
-        if (index == this.length) { return this.value.length }
-        if (index > this.length) { throw "index" }
-        var internalIndex = -1;
-        for (var i=0; i<=index; ++i) {
-            if (UString.isSurrogate(this.value[internalIndex+1])==1) {
-                internalIndex += 2;
+            else if (String.isSurrogate(this[i])==1) {
+                // This is a low surrogate, so a hogh one must precede
+                if (String.isSurrogate(this[i-1])!=2) {
+                    throw "Malformed string: A high surrogate must precede a low one";
+                }
+                // Do not increase length here!
             }
             else {
-                internalIndex += 1;
+                ++this.uLength_alreadyKnown;
             }
         }
-        return internalIndex;
-    },
-    internalIndexToIndex: function(internalIndex) {
-        // For an index of thee internal this.value, returns an index
-        // indicating the position of the corresponding unicode
-        // character in the sequence of unicode characters.
-        // internalIndex is allowed to point to a low or a high
-        // surrogate.
-        // If internalIndex is smaller than 0, an error is thrown
-        // If internalIndex is larger tahen this.value.length,
-        // an error is thrown
-        if (index < 0) { throw "negative index not accepted" }
-        if (index == this.value.length) { return this.length }
-        if (index > this.value.length) { throw "string is shorter" }
-        var index = -1;
-        for (var i=0; i<=internalIndex; ++i) {
-            if (UString.isSurrogate(this.value[i])!=1) {
-                // Increase the internal index only if the current
-                // character is not a low surrogate. This causes
-                // surrogate pairs to be count only once.
-                ++index;
-            }
-        }
-        return index;
-    },
+    }
+    return this.uLength_alreadyKnown;
+});
+String.prototype.uCharAt = function(index) {
+    // Returns a string value
+    var index16 = this.index_UTo16(index);
+    if (String.isSurrogate(this.charAt(index16))) {
+        // Here we relie on the fact that this.index_UTo16 always
+        // points to the first surrogate, never to the second.
+        return this.substring(index16,index16+2);
+    }
+    else {
+        return this.charAt(index16);
+    }
+}
+String.prototype.uCharCodeAt = function(index) {
+    // Returns an integer
+    var index16 = this.index_UTo16(index);
+    if (String.isSurrogate(this.charAt(index16))) {
+        var hi = this.charCodeAt(index16);  
+        var low = this.charCodeAt(index16+1);  
+        return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;  
+    }
+    else {
+        return this.charCodeAt(index16);
+    }
+}
+String.prototype.uIndexOf = function(searchString, position) {
+    var position16 = (position!==undefined) ? this.index_UTo16(position) : undefined;
+    var index16 = this.indexOf(searchString, position16);
+    if (index16 == -1) { return -1 }
+    else { return this.index_16ToU(index16) }
+}
+String.prototype.uLastIndexOf = function(searchString, position) {
+    var position16 = (position!==undefined) ?  this.index_UTo16(position) : undefined;
+    var index16 = this.lastIndexOf(searchString, position16);
+    if (index16 == -1) { return -1 }
+    else { return this.index_16ToU(index16) }
+}
+String.prototype.uSlice = function(start, end) {
+    if (end === undefined) { end = this.uLength }
+    if (isNaN(start)) { start = 0 }
+    if (isNaN(end)  ) { end   = 0 }
+    if (start < 0) { start = this.uLength + start }
+    if (end   < 0) { end   = this.uLength + end   }
+    if (start < 0) { start = 0 }
+    if (end   < 0) { end   = 0 }
+    if (start > this.uLength) { start = this.uLength }
+    if (end   > this.uLength) { end   = this.uLength }
+    var start16 = this.index_UTo16(start);
+    var end16 = this.index_UTo16(end);
+    return this.slice(start16, end16);
+}
+String.prototype.uSubstring = function(start, end) {
+    if (end === undefined) { end = this.uLength }
+    if (isNaN(start) || start<0) { start = 0 }
+    if (isNaN(end)   || end<0  ) { end   = 0 }
+    if (start > this.uLength) { start = this.uLength }
+    if (end   > this.uLength) { end   = this.uLength }
+    var start16 = this.index_UTo16(start);
+    var end16 = this.index_UTo16(end);
+    return this.substring(start16, end16);
 }
 
-UString.fromCharCode = function(codePt) {
+String.prototype.index_UTo16 = function(indexU) {
+    // For a given unicode character based index, the corresponding 
+    // UTF16 index is returned.
+    // If the character at index is from a higher plane, the
+    // internal index of its first surrogate is returned, i.e.
+    // the index of its high surrogate.
+    // If index is less than 0, an error is thrown
+    // If index is larger than this.uLength, an error is
+    // thrown
+    if (indexU < 0) { throw "negative index not accepted" }
+    if (indexU == this.uLength) { return this.length }
+    if (indexU > this.uLength) { throw "index out of range" }
+    var index16 = -1;
+    for (var i=0; i<=indexU; ++i) {
+        if (String.isSurrogate(this[index16+1])==1) {
+            index16 += 2;
+        }
+        else {
+            index16 += 1;
+        }
+    }
+    return index16;
+}
+String.prototype.index_16ToU = function(index16) {
+    // For an UTF16 based index, returns an index
+    // indicating the position of the corresponding unicode
+    // character in the sequence of unicode characters.
+    // index is allowed to point to a low or a high
+    // surrogate.
+    // If index is smaller than 0, an error is thrown
+    // If index is larger tahen this.value.length,
+    // an error is thrown
+    if (index16 < 0) { throw "negative index not accepted" }
+    if (index16 == this.length) { return this.uLength }
+    if (index16 > this.length) { throw "string is shorter" }
+    var indexU = -1;
+    for (var i=0; i<=index16; ++i) {
+        if (String.isSurrogate(this[i])!=1) {
+            // Increase the internal index only if the current
+            // character is not a low surrogate. This causes
+            // surrogate pairs to be count only once.
+            ++indexU;
+        }
+    }
+    return indexU;
+}
+
+String.uFromCharCode = function(codePt) {
     // Returns a string value, not an object
     result = "";
     for (var i=0; i<arguments.length; ++i) {
@@ -161,10 +152,10 @@ UString.fromCharCode = function(codePt) {
     }
     return result;
 }
-UString.isSurrogate = function(character) {
-    return UString.isSurrogateCode(character.charCodeAt(0));
+String.isSurrogate = function(character) {
+    return String.isSurrogateCode(character.charCodeAt(0));
 }
-UString.isSurrogateCode = function(characterCode) {
+String.isSurrogateCode = function(characterCode) {
     // Returns 0 if characterCode is not the codepoint of a surrogate,
     // 1 if it is the codepoint of a low surrogate and 2 if it is the
     // codepoint of a high surrogate.
@@ -177,10 +168,5 @@ UString.isSurrogateCode = function(characterCode) {
     else {
         return 0;
     }
-}
-
-
-String.prototype.toUString = function() {
-    return new UString(this.toString());
 }
 
