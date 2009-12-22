f1fd9ae5646d070ef994fd9ca8c883a19ee7a7cd
Urs Holzer <urs@andonyar.com>
2009-12-22 11:09:38 +0100

    Added java class URLSetPolicy
    It is needed to run jomdoc and its other libraries with elevated
    privileges. This is not because it needs to access files in its own
    jar files since the URLClassLoader allows that anyway. It seems that
    it is needed by Saxon to register itself globally.
    
    The class file is included in the repository as well, so one does not
    need to recompile when switching branches.

---
+++ approach1/src_common/java/com/andonyar/gemse/security/URLSetPolicy.java
@@ -0,0 +1,60 @@
+/*
+ * 
+ */
+package com.andonyar.gemse.security;
+
+import java.net.URL;
+import java.security.CodeSource;
+import java.security.Permission;
+import java.security.PermissionCollection;
+import java.security.Permissions;
+import java.security.Policy;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ */
+public class URLSetPolicy extends Policy {
+    private Permissions   permissions = new Permissions();
+    private Policy        outerPolicy;
+    private Set<String>   urls = new HashSet<String>(); // Or should we use HashSet<URL>?
+
+    public PermissionCollection getPermissions(CodeSource codesource) {
+        PermissionCollection pc = outerPolicy != null ?
+                outerPolicy.getPermissions(codesource) :
+                new Permissions();
+        
+        URL url = codesource.getLocation();
+        if (url != null) {
+            String s = url.toExternalForm();
+            if (urls.contains(s) || "file:".equals(s)) {
+                Enumeration<Permission> e = permissions.elements();
+                while (e.hasMoreElements()) {
+                    pc.add(e.nextElement());
+                }
+            }
+        }
+        
+        return pc;
+    }
+    
+    /**
+     * Sets the outer policy so that we can defer to it for code sources that
+     * we are not told about.
+     * 
+     * @param policy
+     */
+    public void setOuterPolicy(Policy policy) {
+        outerPolicy = policy;
+    }
+    
+    public void addPermission(Permission permission) {
+        permissions.add(permission);
+    }
+    
+    public void addURL(URL url) {
+        urls.add(url.toExternalForm());
+    }
+}
